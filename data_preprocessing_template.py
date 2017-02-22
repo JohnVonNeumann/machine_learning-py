96c70d687fb0d03e54d9011cd6a9dc5632bbf51c
JohnVonNeumann <louiswillcock@gmail.com>
2017-02-23 09:17:00 +1100

    initial commit, as stated briefly in the readme, this is the python side of my ML adventures, I will probably end up moving repo's around and cleaning things up as I go, I will be commiting code as I progress and changing things as I develop understanding, this commit contains some basic data proprocessing steps, largely revolving around creating an indep var array, and a dep var array.

---
+++ data_preprocessing_template.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 22 21:18:11 2017
+
+@author: lw
+
+Data Preprocessing
+
+Importing the libraries
+"""
+
+#Import Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Import Dataset
+dataset = pd.read_csv('Data.csv')
+

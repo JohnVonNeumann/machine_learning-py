237980ab51a2581ea49470b62a09b83efe2e8d66
JohnVonNeumann <louiswillcock@gmail.com>
2017-02-24 14:21:40 +1100

    in this commit, ive started to add more of the preprocessing steps required to start wrangling the data, in particular, pulling in sklearn, an ml library that is heavily used in industry, im using the proprocessing methods currently, im not sure if my placement of the call is correct, is it better to call in libs on the line you use them, or all at the top? ive also come back around and added commments, im making a concious effort to tag up all my code with comments, i want it to be a proper habit

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -16,12 +16,12 @@ import pandas as pd
 import sklearn
 
 #Import Dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values 
+dataset = pd.read_csv('Data.csv') #use pandas to read csv data from a file
+X = dataset.iloc[:, :-1].values #independent variable vector, so, country, age, salary
+y = dataset.iloc[:, 3].values #dependent variable vector, purchased
 
 #Sorting out missing data values
-from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import Imputer 
 imputer = Imputer(missing_values = 'NaN', strategy="mean", axis=0) # for all NaN cells, replace with columnar mean
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+imputer = imputer.fit(X[:, 1:3]) #fits the imputer method onto the vector/array we need to be fixed
+X[:, 1:3] = imputer.transform(X[:, 1:3]) #transforms any missing data points, based on our choices on line 25

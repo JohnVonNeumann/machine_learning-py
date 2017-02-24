092a1afa23a02614236ee49897e2546daa973818
JohnVonNeumann <louiswillcock@gmail.com>
2017-02-24 14:45:02 +1100

    final commits and changes to comments and whatnot, removed a useless call to import sklearn, im probabyl gonna give this all another crack soon with different replacement params, ie give median and mode a crack, and with a proper dataset, moving onto r implementation now

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -13,15 +13,15 @@ Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import sklearn
 
 #Import Dataset
 dataset = pd.read_csv('Data.csv') #use pandas to read csv data from a file
-X = dataset.iloc[:, :-1].values #independent variable vector, so, country, age, salary
-y = dataset.iloc[:, 3].values #dependent variable vector, purchased
+X = dataset.iloc[:, :-1].values #independent variable vector, so, country, age, salary, the number is a stop point, -1 means the first 
+                                #from the right will not be included, as we calc UPTO the stop point, not including
+y = dataset.iloc[:, 3].values #dependent variable vector, purchased, in this instance
 
 #Sorting out missing data values
 from sklearn.preprocessing import Imputer 
 imputer = Imputer(missing_values = 'NaN', strategy="mean", axis=0) # for all NaN cells, replace with columnar mean
-imputer = imputer.fit(X[:, 1:3]) #fits the imputer method onto the vector/array we need to be fixed
+imputer = imputer.fit(X[:, 1:3]) #fits the imputer method onto the vector/array we need to be fixed, in this case, 
 X[:, 1:3] = imputer.transform(X[:, 1:3]) #transforms any missing data points, based on our choices on line 25

741f2308890fa37b49894f219c875ae2ec7a5be3
JohnVonNeumann <louiswillcock@gmail.com>
2017-02-26 09:39:20 +1100

    my attempt at first use of the linear regression lib, not sure ive done this right, i think im on the right path but the way ive called it has something wrong with it, I've lost the X_train var from the var explorer, which is a pretty glaring indication that ive fucked soemthing up along the way, ill get the solution next

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -20,3 +20,7 @@ Y = dataset.iloc[:, 1].values #dependent variable vector
 from sklearn.cross_validation import train_test_split #train_test_split is used to segregate test and train data in order to ensure we have datasets that minimise the risk of us overfitting our models
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0) # here we crtreate test and train sets, we outline set size, and becuase it always equals 1, we dont need to use the other number.
 
+# Fitting the simple linear regression to training set
+from sklearn.linear_model import LinearRegression # import the LR functions
+X_train_LR = LinearRegression() # create a var for the methods to reside in
+X_train = X_train_LR.fit(X_train, Y_train, sample_weight = None ) # attempt to apply the methods to the training sets, using docsets
